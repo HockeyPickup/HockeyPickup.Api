3c7cbc9214070f385b80cd3571a0a624922293e0
Brett Morrison <b@brettmorrison.com>
2024-11-22 14:55:21 -0800

    Updated password settings to be more compatible with v1

--- HockeyPickup.Api/Program.cs
+++ HockeyPickup.Api/Program.cs
@@ -158,15 +158,22 @@ public class Program
         builder.Services.AddIdentity<AspNetUser, AspNetRole>(options =>
         {
             options.SignIn.RequireConfirmedEmail = true;
+            options.Password.RequiredLength = 6;
             options.Password.RequireDigit = true;
-            options.Password.RequireLowercase = true;
-            options.Password.RequireUppercase = true;
-            options.Password.RequireNonAlphanumeric = true;
-            options.Password.RequiredLength = 8;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
         })
         .AddEntityFrameworkStores<HockeyPickupContext>()
         .AddUserStore<CustomUserStore>()
         .AddDefaultTokenProviders();
+
+        // TODO: Remove this after v1 retirement
+        // Temporarily disable for transition period.
+        builder.Services.Configure<SecurityStampValidatorOptions>(options =>
+        {
+            options.ValidationInterval = TimeSpan.MaxValue;
+        });
 
         builder.Services.AddAuthentication(options =>
         {
